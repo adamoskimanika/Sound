c65fb4e11f70aaf0197a3d88741784e6d459fedb
TIAGO MANIKA ADAMOSKI <adamoskim@gmail.com>
2023-03-29 15:26:54 -0300

    finalizado

--- app/src/main/java/com/example/sound/MainActivity.java
+++ app/src/main/java/com/example/sound/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.sound;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        Button button = findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            MediaPlayer   mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.beep);
+               mediaPlayer.start();
+            }
+        });
     }
 }
